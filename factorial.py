092103747874d92672da45e5cab298c47b91e04b
Paul Riechers <pmriechers@ucdavis.edu>
2016-02-03 17:51:51 -0800

    PMR: updated py factorial function to deal with different data types

--- factorial.py
+++ factorial.py
@@ -1,14 +1,23 @@
-def factorial(PositiveInt):
+def factorial(positive_int):
     """
-    Computes the factorial of a positive integer
+    Computes the factorial of a positive integer.
+    Integers, integer floats, and integer complex numbers with zero imaginary parts are accepted input.
     """
     
-    if PositiveInt == int(PositiveInt):
-        if PositiveInt > 0:
-            Prod = 1
-            for n in xrange(int(PositiveInt)):
-                Prod *= n+1
-            return Prod
+    if type(positive_int) == complex: 
+        if positive_int.imag == 0:
+            positive_int = positive_int.real
+        else:
+            raise NotImplementedError("The factorial is only defined for positive integers, not complex values with nonzero imaginary part")
+    if type(positive_int) == str:
+         raise NotImplementedError("The factorial is only defined for positive integers, not strings")
+    #____________________________________
+    if positive_int == int(positive_int):
+        if positive_int > 0:
+            fac_prod = 1
+            for n in xrange(int(positive_int)):
+                fac_prod *= n+1
+            return fac_prod
         else: 
             raise NotImplementedError("The factorial is only defined for positive integers")
     else: 
