db4d6fa392d1cf032e2861fb6d0966a6f79dde5f
Paul Riechers <pmriechers@ucdavis.edu>
2016-02-03 17:09:16 -0800

    Added python factorial function

---
+++ factorial.py
@@ -0,0 +1,15 @@
+def factorial(PositiveInt)
+    """
+    Computes the factorial of a positive integer
+    """
+    
+    if PositiveInt == int(PositiveInt):
+        if PositiveInt > 0:
+            Prod = 1
+            for n in xrange(int(PositiveInt)):
+                Prod *= n+1
+            return Prod
+        else: 
+            raise NotImplementedError("The factorial is only defined for positive integers")
+    else: 
+            raise NotImplementedError("The factorial is only defined for positive integers")
